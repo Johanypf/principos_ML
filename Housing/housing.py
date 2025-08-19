408ad717e515186592af87a122c44e0adc9ff192
Johanypf <johanyflorez@gmail.com>
2025-08-18 21:39:18 -0500

    update2

--- Housing/housing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from mlxtend.plotting import scatterplotmatrix
-
-df = pd.read_csv('./housing.data.txt', sep=r'\s+',engine='python')
-df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS','NOX', 'RM', 'AGE', 'DIS', 'RAD',
-            'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
-print(df.head())
